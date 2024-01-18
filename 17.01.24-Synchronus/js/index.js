5878272c4654d54d33acdf8e056da1ddc84c9ae8
NewNobbie <116767117+NewNobbie@users.noreply.github.com>
2024-01-18 07:45:43 -0500

    a

--- 17.01.24-Synchronus/js/index.js
+++ 17.01.24-Synchronus/js/index.js
@@ -77,7 +77,37 @@ function showHtmlArreglo(array){
     //     <p> Edad: ${edad}</p>
     //     <p> Nivel de Ingles: ${englishLevel}</p>
     // `
+//4.  FetchAPI para consumir recursos desde una API PUBLICA (internet)
+
+const loadAPIBtn = document.querySelector('#loadAPI');
+loadAPIBtn.addEventListener('click', ()=>{
+    const contenidoImages = document.querySelector('#contenido');
+    contenidoImages.style.display = 'flex';
+    getDataApiReal()
+});
+
+function getDataApiReal(){
+    fetch('https://picsum.photos/v2/list')
+    .then(rtaPicsum => {
+        // console.log(rtaPicsum);
+        return rtaPicsum.json();
+    })
+    .then(dataPic =>{
+        // console.log(dataPic);
+        injectingImages(dataPic);
+    })
 }
 
+function injectingImages(images){
+    const contenidoImages = document.querySelector('#contenido');
+    let html = '';
+    images.forEach((image) => {
+        const {author, download_url}= image;
+        html += `
+        <div><p> Autor: ${author} </p>
+        <img src="${download_url}" width = "300px"></div>
+        `;
+    });
+    contenidoImages.innerHTML = html;
+}
 
-//4.  FetchAPI para consumir recursos desde una API PUBLICA (internet)
